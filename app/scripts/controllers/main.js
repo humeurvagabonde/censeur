07e64cbc7e4ae82fd7ec5e7420c93504dfcbb041
François Lagoueyte <francois.lagoueyte@asso-cocktail.fr>
2013-09-06 15:41:00 +0200

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('censeurApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
